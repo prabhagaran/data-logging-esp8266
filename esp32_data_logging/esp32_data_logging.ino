dadd375643719d1368223c86409f6e8624d38a1f
prabhagaran <prabhasurya146@gmail.com>
2025-12-15 16:08:42 +0530

    temperature sensor integrated

--- esp32_data_logging/esp32_data_logging.ino
+++ esp32_data_logging/esp32_data_logging.ino
@@ -3,41 +3,54 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define ONE_WIRE_BUS 2  // DS18B20 data pin
+#define ONE_WIRE_BUS 4   // D2 (SAFE PIN)
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 const char* ssid = "ST - LAB";
 const char* password = "simtestlab";
 
-const char* serverName = "https://script.google.com/macros/s/AKfycbxkEMndc-Mzh_DZRJUoclMKazBu1skBX_NUfYQven5KQXNH_QMNEQUWVkWQ_3J-y8Wa/exec";
+const char* serverName =
+"https://script.google.com/macros/s/AKfycbxkEMndc-Mzh_DZRJUoclMKazBu1skBX_NUfYQven5KQXNH_QMNEQUWVkWQ_3J-y8Wa/exec";
 
 void setup() {
   Serial.begin(115200);
   sensors.begin();
-  WiFi.begin(ssid, password);
 
+  WiFi.begin(ssid, password);
   Serial.print("Connecting");
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+
   Serial.println("\nWiFi Connected");
 }
 
 void loop() {
-   sensors.requestTemperatures(); 
+
+  sensors.requestTemperatures();
+  float temperature = sensors.getTempCByIndex(0);
+
+  if (temperature == DEVICE_DISCONNECTED_C) {
+    Serial.println("DS18B20 not detected");
+    delay(10000);
+    return;
+  }
+
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
     WiFiClientSecure client;
-    client.setInsecure();           // IMPORTANT for HTTPS
-  // Ask sensor to measure
-  
-    float temperature = sensors.getTempCByIndex(0);
-    float humidity = 65.2;
-    float voltage = 3.72;
+    client.setInsecure();
 
-    String url = String(serverName) + "?temp=" + temperature + "&hum=" + humidity + "&volt=" + voltage;
+    float humidity = 65.2;   // placeholder
+    float voltage  = 3.72;   // placeholder
+
+    String url = String(serverName) +
+      "?temp=" + String(temperature, 2) +
+      "&hum="  + String(humidity, 2) +
+      "&volt=" + String(voltage, 2);
 
     http.begin(client, url);
     int httpResponseCode = http.GET();
@@ -51,5 +64,5 @@ void loop() {
     http.end();
   }
 
-  delay(10000);  // send every 10 seconds
+  delay(10000);
 }

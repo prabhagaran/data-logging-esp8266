e9252cb27cdf19481c963e6241c3dabcdbe90d31
prabhagaran <prabhasurya146@gmail.com>
2025-12-15 17:12:52 +0530

    datas logged adde in sheet

--- esp32_data_logging/esp32_data_logging.ino
+++ esp32_data_logging/esp32_data_logging.ino
@@ -8,25 +8,70 @@
 const char* ssid = "ST - LAB";
 const char* password = "simtestlab";
 
-// ---------------- Google Script ----------------
+// ---------------- Google Apps Script ----------------
 const char* serverName =
-"https://script.google.com/macros/s/AKfycbxkEMndc-Mzh_DZRJUoclMKazBu1skBX_NUfYQven5KQXNH_QMNEQUWVkWQ_3J-y8Wa/exec";
+"https://script.google.com/macros/s/AKfycbwejNTJ6hRByPLzRMpkq34dBZomUEnfpHsP6yL574k8EPttZohjJcwGAng4CsyE54dL/exec";
 
 // ---------------- NTP ----------------
-const char* ntpServer = "pool.ntp.org";
+const char* ntpServer = "time.google.com";
 const long gmtOffset_sec = 19800;   // IST
 const int daylightOffset_sec = 0;
 
 // ---------------- DS18B20 ----------------
-#define ONE_WIRE_BUS 4   // D2 (SAFE)
+#define ONE_WIRE_BUS 4   // D2
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
+// ---------------- Alarm ----------------
+#define BUZZER_PIN  D5
+#define ACK_BUTTON  D6
+
+bool alarmActive = false;
+bool rotationDone[3] = {false, false, false};
+
+// Rotation schedule
+int rotHour[3] = {6, 14, 22};
+int rotMin[3]  = {0, 0, 0};
+
+// ---------------- Periodic logging ----------------
+const unsigned long LOG_INTERVAL = 30000; // 30 seconds
+static unsigned long lastLogTime = 0;
+
+// ---------------- Logging helpers ----------------
+void sendToSheet(float temp, const char* event) {
+  if (WiFi.status() != WL_CONNECTED) return;
+
+  WiFiClientSecure client;
+  client.setInsecure();
+  HTTPClient http;
+  http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
+
+  String url = String(serverName) +
+               "?temp=" + String(temp, 2) +
+               "&hum=52" +
+               "&heater=1" +
+               "&fan=1" +
+               "&volt=4.98" +
+               "&event=" + event;
+
+  http.begin(client, url);
+  int code = http.GET();
+  http.end();
+
+  Serial.print("Sheet log (");
+  Serial.print(event);
+  Serial.print(") HTTP: ");
+  Serial.println(code);
+}
+
+// ---------------- Setup ----------------
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  Serial.println("Egg Incubator Starting");
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(ACK_BUTTON, INPUT_PULLUP);
+  digitalWrite(BUZZER_PIN, LOW);
 
   sensors.begin();
 
@@ -39,60 +84,67 @@ void setup() {
   }
   Serial.println("\nWiFi Connected");
 
-  // NTP
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  // NTP (non-blocking safe)
   Serial.print("Syncing time");
-  time_t now = time(nullptr);
-  while (now < 100000) {
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+
+  unsigned long startAttempt = millis();
+  while (time(nullptr) < 100000 && millis() - startAttempt < 20000) {
     delay(500);
     Serial.print(".");
-    now = time(nullptr);
   }
-  Serial.println("\nTime synced");
+
+  if (time(nullptr) < 100000)
+    Serial.println("\nNTP sync failed, continuing");
+  else
+    Serial.println("\nTime synced");
 }
 
+// ---------------- Loop ----------------
 void loop() {
 
   // -------- Time --------
   time_t now = time(nullptr);
-  struct tm* timeinfo = localtime(&now);
+  struct tm* t = localtime(&now);
 
-  char timeStr[25];
-  strftime(timeStr, sizeof(timeStr),
-           "%Y-%m-%d %H:%M:%S", timeinfo);
+  int hour = t->tm_hour;
+  int minute = t->tm_min;
 
   // -------- Temperature --------
   sensors.requestTemperatures();
   float tempC = sensors.getTempCByIndex(0);
+  if (tempC == DEVICE_DISCONNECTED_C) return;
 
-  if (tempC == DEVICE_DISCONNECTED_C) {
-    Serial.println("DS18B20 ERROR");
-    delay(10000);
-    return;
+  // -------- Egg rotation alarm --------
+  for (int i = 0; i < 3; i++) {
+    if (hour == rotHour[i] && minute == rotMin[i] && !rotationDone[i]) {
+      rotationDone[i] = true;
+      alarmActive = true;
+      digitalWrite(BUZZER_PIN, HIGH);
+      sendToSheet(tempC, "ROTATION_ALARM");
+    }
   }
 
-  // -------- Send to Google Sheets --------
-  if (WiFi.status() == WL_CONNECTED) {
-
-    WiFiClientSecure client;
-    client.setInsecure();
-    HTTPClient http;
-
-    String url = String(serverName) +
-                 "?temp=" + String(tempC, 2);
-
-    http.begin(client, url);
-    int httpCode = http.GET();
-
-    Serial.print("Time: ");
-    Serial.print(timeStr);
-    Serial.print(" | Temp: ");
-    Serial.print(tempC, 2);
-    Serial.print(" °C | Sheet Status: ");
-    Serial.println(httpCode);
-
-    http.end();
+  // -------- ACK button --------
+  if (alarmActive && digitalRead(ACK_BUTTON) == LOW) {
+    alarmActive = false;
+    digitalWrite(BUZZER_PIN, LOW);
+    sendToSheet(tempC, "ROTATION_ACK");
+    delay(400); // debounce
   }
 
-  delay(10000);  // every 10 sec
+  // -------- Reset rotation flags at midnight --------
+  if (hour == 0 && minute == 0) {
+    rotationDone[0] = rotationDone[1] = rotationDone[2] = false;
+  }
+
+  // -------- Periodic process logging (30s) --------
+  unsigned long nowMillis = millis();
+  if (nowMillis - lastLogTime >= LOG_INTERVAL) {
+    lastLogTime = nowMillis;
+    sendToSheet(tempC, "");
+    Serial.println("Periodic process log");
+  }
+
+  delay(500);
 }

abe98d0575244a07abca03e5b93cb5c6276f82e3
prabhagaran <prabhasurya146@gmail.com>
2025-12-15 16:22:17 +0530

    NTP time added

--- esp32_data_logging/esp32_data_logging.ino
+++ esp32_data_logging/esp32_data_logging.ino
@@ -2,67 +2,97 @@
 #include <ESP8266HTTPClient.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <time.h>
 
-#define ONE_WIRE_BUS 4   // D2 (SAFE PIN)
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
-
+// ---------------- WiFi ----------------
 const char* ssid = "ST - LAB";
 const char* password = "simtestlab";
 
+// ---------------- Google Script ----------------
 const char* serverName =
 "https://script.google.com/macros/s/AKfycbxkEMndc-Mzh_DZRJUoclMKazBu1skBX_NUfYQven5KQXNH_QMNEQUWVkWQ_3J-y8Wa/exec";
 
+// ---------------- NTP ----------------
+const char* ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 19800;   // IST
+const int daylightOffset_sec = 0;
+
+// ---------------- DS18B20 ----------------
+#define ONE_WIRE_BUS 4   // D2 (SAFE)
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
 void setup() {
   Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("Egg Incubator Starting");
+
   sensors.begin();
 
+  // WiFi
+  Serial.print("Connecting WiFi");
   WiFi.begin(ssid, password);
-  Serial.print("Connecting");
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-
   Serial.println("\nWiFi Connected");
+
+  // NTP
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  Serial.print("Syncing time");
+  time_t now = time(nullptr);
+  while (now < 100000) {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("\nTime synced");
 }
 
 void loop() {
 
-  sensors.requestTemperatures();
-  float temperature = sensors.getTempCByIndex(0);
+  // -------- Time --------
+  time_t now = time(nullptr);
+  struct tm* timeinfo = localtime(&now);
 
-  if (temperature == DEVICE_DISCONNECTED_C) {
-    Serial.println("DS18B20 not detected");
+  char timeStr[25];
+  strftime(timeStr, sizeof(timeStr),
+           "%Y-%m-%d %H:%M:%S", timeinfo);
+
+  // -------- Temperature --------
+  sensors.requestTemperatures();
+  float tempC = sensors.getTempCByIndex(0);
+
+  if (tempC == DEVICE_DISCONNECTED_C) {
+    Serial.println("DS18B20 ERROR");
     delay(10000);
     return;
   }
 
+  // -------- Send to Google Sheets --------
   if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
+
     WiFiClientSecure client;
     client.setInsecure();
-
-    float humidity = 65.2;   // placeholder
-    float voltage  = 3.72;   // placeholder
+    HTTPClient http;
 
     String url = String(serverName) +
-      "?temp=" + String(temperature, 2) +
-      "&hum="  + String(humidity, 2) +
-      "&volt=" + String(voltage, 2);
+                 "?temp=" + String(tempC, 2);
 
     http.begin(client, url);
-    int httpResponseCode = http.GET();
+    int httpCode = http.GET();
 
-    if (httpResponseCode > 0) {
-      Serial.println("Data sent successfully");
-    } else {
-      Serial.println("Error sending data");
-    }
+    Serial.print("Time: ");
+    Serial.print(timeStr);
+    Serial.print(" | Temp: ");
+    Serial.print(tempC, 2);
+    Serial.print(" °C | Sheet Status: ");
+    Serial.println(httpCode);
 
     http.end();
   }
 
-  delay(10000);
+  delay(10000);  // every 10 sec
 }
